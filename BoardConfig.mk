2fbdc1eb6fb89d30aaf448b253ebed5072168dfc
DerfElot <frederic.koch@gmail.com>
2017-01-05 19:53:17 +0100

    Amami: Revert: add memory parameter
    Not needed for 3.10 kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,5 +18,3 @@ include device/sony/rhine-common/PlatformConfig2.mk
 TARGET_BOOTLOADER_BOARD_NAME := D5503
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=amami
-BOARD_KERNEL_CMDLINE += mem=1767M
-
